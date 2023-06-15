843e0e34f8d99e3e5b4a69c773d7646cb4884431
Tyler Kirkwood <tkirkwood@myseneca.ca>
2023-06-15 08:48:13 -0400

    updating some things

--- checkpoint6/bash-scripts/network_config.sh
+++ checkpoint6/bash-scripts/network_config.sh
@@ -6,14 +6,11 @@
 
 RG_NAME="Student-RG-954276"     # your student group
 LOCATION="canadacentral"    # your location
-ID="99"          #unique ID assigned to you
-
-Student_vnet_name="Student-954276-vnet"
-Student_vnet_address="10.23.205.0/24"
+ID="61"          #unique ID assigned to you
+Student_vnet_name="Student-964259-vnet"
+Student_vnet_address="10.18.43.0/24"
 Client_Subnet_name="Virtual-Desktop-Client"
-Client_Subnet_address="10.23.205.0/24"
-
-
+Client_Subnet_address="10.18.43.0/24
 # ---------------------------------------------------
 # Networking - Virtual Networks
 Router_vnet_name="Router-$ID"
